68e9ac4313994a31b05cd2d499de0ce54b5693d8
leojpod <leo.jpod@gmail.com>
2015-03-30 18:38:39 +0000

    it works! but needs some cleaning

--- addon/components/dnd-item.js
+++ addon/components/dnd-item.js
@@ -1,15 +1,92 @@
 import Ember from 'ember';
+
+import Droppable from 'ember-drag-drop/mixins/droppable';
+// import DraggableObjectTarget from 'ember-drag-drop/components/draggable-object-target';
+
+// import DraggableObjectTarget from 'ember-drag-drop-list/components/draggable-object-target';
+
 import layout from '../templates/components/dnd-item';
 
-const {Logger} = Ember;
+const {computed, Logger, observer, on} = Ember;
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(Droppable, {
+  // {
   layout: layout,
+  itemIdx: computed('item', 'list', function () {
+    return this.get('list').indexOf(this.get('item'));
+  }),
+  draggingObject: computed('list.@each.isDraggingObject', function () {
+    let draggingObject;
+    const list = this.get('list');
+    draggingObject = list.findBy('isDraggingObject', true);
+    Logger.debug('draggingObject -> ', draggingObject);
+    return draggingObject;
+  }),
+  dragIdx: computed('list', 'draggingObject', function () {
+    const list = this.get('list'),
+      draggingObject = this.get('draggingObject'),
+      dragIdx = list.indexOf(draggingObject);
+    Logger.debug('dragIdx -> ', dragIdx);
+    return dragIdx;
+  }),
+  isMovingUp: computed('itemIdx', 'dragIdx', 'isDragOver', function () {
+    return this.get('isDragOver') && this.get('dragIdx') !== -1 &&
+      this.get('itemIdx') < this.get('dragIdx');
+  }),
+  isMovingDown: computed('itemIdx', 'dragIdx', 'isDragOver', function () {
+    return this.get('isDragOver') && this.get('dragIdx') !== -1 &&
+      this.get('itemIdx') > this.get('dragIdx');
+  }),
+
   // list: null,
+  // draggingObjectObs: observer('isDraggingObject', function () {
+  //   Logger.debug('isDraggingObject? -> ', this.get('isDraggingObject'));
+  // }).on('init'),
+  handlePayload(payload) {
+    const obj = this.get('coordinator').getObject(payload,{target: this});
+    Logger.debug('handlePayload obj -> ', obj);
+    this.send('rearrangeList',obj,{target: this});
+  },
+  handleDrop(event) {
+    const dataTransfer = event.dataTransfer,
+      payload = dataTransfer.getData("Text");
+    Logger.debug('handleDrop -> ', payload);
+    this.handlePayload(payload);
+  },
+  acceptDrop(event) {
+    Logger.debug('acceptDrop -> ', event);
+    this.handleDrop(event);
+  },
+  onDragEnter: on('dragEnter', function () {
+    Logger.debug('dragEnter!');
+    this.set('isDragOver', true);
+  }),
+  // onDragLeave: on('dragLeave', function () {
+  //   this.set('isDragOver', false);
+  // }),
+  noMoreDrag: on('dragStop', 'drop', 'dragLeave', function (event) {
+    Logger.debug('noMoreDrag -> ', event);
+    this.set('isDragOver', false);
+  }),
+  // dragOver(event) {
+  //   Logger.debug('dragOver -> ', event);
+  //   const payload = event.dataTransfer.getData("Text");
+  //   Logger.debug('payload ->', payload);
+  //   Logger.debug('coordinator -> ', this.get('coordinator'));
+  //   const  obj = this.get('coordinator').getObject(payload, {target: this});
+  //
+  //   const dragIdx = this.get('list').indexOf(obj);
+  //   this.set('dragIdx', dragIdx);
+  // },
   actions: {
+    acceptForDrop() {
+      const hashId = this.get('coordinator.clickedId');
+      Logger.debug('acceptForDrop -> ', hashId);
+      this.handlePayload(hashId);
+    },
     rearrangeList(droppedObject, options) {
       const list = this.get('list'),
-        targetIdx = list.indexOf(options.target.item),
+        targetIdx = this.get('itemIdx'),
         originIdx = list.indexOf(droppedObject);
       Logger.debug('list -> ', list.length);
       Logger.debug('from -> ', originIdx, ' to ', targetIdx);
@@ -26,12 +103,3 @@ export default Ember.Component.extend({
     }
   }
 });
-
-/*
-0 - A                           0 - A  -> move it up                                .
-1 - B                           1 - B                                 .
-2 - C->take C show placeholder  2 - _                                                         .
-3 - D                           3 - D                              .
-4 - E                           4 - E                               .
-
-*/
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,16 +1,25 @@
 import Ember from 'ember';
 
-// const {computed, Logger, Object: Obj} = Ember;
+const {computed, on, Object: Obj} = Ember;
 
 export default Ember.Controller.extend({
+  MessageObject: Obj.extend({ type: null, message: null}),
   messages: [
-    { type: "info", message: "this is an info message"},
-    { type: "info", message: "this is another info message"},
-    { type: "warning", message: "this is an warning message"},
-    { type: "error", message: "this is an error message"},
-    { type: "danger", message: "this is an danger message"},
-    { type: "danger", message: "this is another danger message"},
-    { type: "error", message: "this is another error message"},
-    { type: "warning", message: "this is another warning message"}
-  ]
+    // { type: "info", message: "this is an info message"},
+    // { type: "info", message: "this is another info message"},
+    // { type: "warning", message: "this is an warning message"},
+    // { type: "error", message: "this is an error message"},
+    // { type: "danger", message: "this is an danger message"}
+  ],
+
+  initMessaged: on('init', function () {
+    this.messages.pushObject(this.MessageObject.create({ type: "info", message: "this is an info message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "info", message: "this is another info message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "warning", message: "this is an warning message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "danger", message: "this is an danger message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "error", message: "this is an error message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "danger", message: "this is another danger message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "error", message: "this is another error message"}));
+    this.messages.pushObject(this.MessageObject.create({ type: "warning", message: "this is another warning message"}));
+  })
 });
